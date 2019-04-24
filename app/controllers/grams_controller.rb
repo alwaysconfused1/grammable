99a96c29286af37839fdcbf0405540a5d9bf123e
Wilson Horrell <wilsonhorrell@gmai.com>
2019-04-24 20:24:44 +0000

    Added the ability to delete grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,18 +1,18 @@
 class GramsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
 
-def update
-@gram = Gram.find_by_id(params[:id])
-return render_not_found if @gram.blank?
+	def update
+		@gram = Gram.find_by_id(params[:id])
+		return render_not_found if @gram.blank?
 
-@gram.update_attributes(gram_params)
+		@gram.update_attributes(gram_params)
 
-if @gram.valid?
-	redirect_to root_path
-else render :edit, status: :unprocessable_entity
-end
+		if @gram.valid?
+			redirect_to root_path
+		else render :edit, status: :unprocessable_entity
+		end
 
-end
+	end
 
 	def index
 	end
